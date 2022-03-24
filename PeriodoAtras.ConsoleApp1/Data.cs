7343fd676435a821fa5a7e5cb208507991d5dfbc
nedzie <marcos-elivelton@hotmail.com>
2022-03-24 17:26:10 -0300

    Add project files.

---
+++ PeriodoAtras.ConsoleApp1/Data.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace PeriodoAtras.ConsoleApp1
+{
+    public partial class Data
+    {
+        public DateTime dateHoje;
+        public DateTime dataInformada;
+
+        public string Mensagem(TimeSpan dias)
+        {
+            if (dias.TotalDays < 0)
+                return ($"Futuro?");
+            if (dias.TotalDays < 1)
+            {
+                if (dias.TotalMinutes < 60 && dias.TotalSeconds > 60)
+                {
+                    return ($"{dias.TotalMinutes % 60:F0} minutos atrás");
+                }
+                else if (dias.TotalSeconds < 60)
+                {
+                    return ($"{dias.TotalSeconds % 60:F0} segundos atrás");
+                }
+                return ($"{dias.Hours % 24} horas atrás");
+            }
+
+
+
+            if (dias.Days < 7)
+            {
+                IntParaStringDias dia = (IntParaStringDias)dias.Days;
+                if (dias.Days > 1)
+                {
+                    return ($"{dia} dia se passaram");
+                }
+                else
+                    return ($"{dia} dias se passaram");
+            }
+            else if (dias.Days >= 7 && dias.Days < 30)
+            {
+                IntParaStringDias semanas;
+                IntParaStringDias dia = (IntParaStringDias)(dias.Days % 7);
+                if (dias.Days / 7 == 2)
+                    semanas = (IntParaStringDias)Enum.Parse(typeof(IntParaStringDias), "duas");
+
+                else
+                    semanas = (IntParaStringDias)(dias.Days / 7);
+
+                if (dias.Days / 7 == 0)
+                {
+                    return ($" se passaram {semanas} semana(s)");
+                }
+                return ($"se passaram {semanas} semana(s) e {dia} dia(s)");
+            }
+            else if (dias.Days >= 30 && dias.Days < 360)
+            {
+                IntParaStringDias dia = (IntParaStringDias)(((dias.Days % 30) % 7));
+                IntParaStringDias semanas;
+                IntParaStringDias meses = (IntParaStringDias)(dias.Days / 30);
+
+
+                if ((dias.Days % 30) / 7 == 2)
+                    semanas = (IntParaStringDias)Enum.Parse(typeof(IntParaStringDias), "duas");
+
+                else
+                    semanas = (IntParaStringDias)(dias.Days / 7);
+
+
+                if ((dias.Days % 30) / 7 == 0)
+                {
+                    if ((dias.Days % 30) / 7 == 0)
+                    {
+                        return ($"se passaram {meses} mes(es)");
+                    }
+                    return ($"se passaram {meses} mes(es) e {semanas} semana(s)");
+                }
+
+
+                return ($"{meses} mese(s) se passaram e {semanas} semana(s) e {dia} dia(s)");
+            }
+            else if (dias.Days >= 360)
+            {
+                IntParaStringDias dia = (IntParaStringDias)(((dias.Days % 360) / 30 % 7));
+                IntParaStringDias semanas = (IntParaStringDias)((dias.Days % 360) % 30 / 7);
+                IntParaStringDias meses = (IntParaStringDias)(dias.Days % 360 / 30);
+                IntParaStringDias anos = (IntParaStringDias)(dias.Days / 360);
+
+                if ((dias.Days % 360) % 30 / 7 == 2)
+                    semanas = (IntParaStringDias)Enum.Parse(typeof(IntParaStringDias), "duas");
+
+                else
+                    semanas = (IntParaStringDias)((dias.Days % 360) % 30 / 7);
+
+
+
+
+                if ((dias.Days % 360) / 30 % 7 == 0 && (dias.Days % 360) % 30 / 7 == 0 && (dias.Days % 360 / 30) == 0)
+
+                    return ($" se passaram {anos} ano(s)");
+
+                else if ((dias.Days % 360) / 30 % 7 == 0 && (dias.Days % 360) % 30 / 7 == 0)
+                    return ($" se passaram {anos} ano(s) e {meses} mese(s)");
+                else if ((dias.Days % 360) / 30 % 7 == 0)
+                    return ($"se passaram {dia} ano(s) e {meses} mese(s) e {semanas} semana(s)");
+
+                return ($"se passaram {anos} ano(s) e {meses} mese(s) e {semanas} semana(s) e {dia} dia(s)");
+            }
+            return "";
+        }
+    }
+
+
+}
+
+
---
+++ PeriodoAtras.ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PeriodoAtras.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Data datas = new();
+            TimeSpan dias = new TimeSpan();
+<<<<<<< HEAD
+            datas.dateHoje = DateTime.Now;
+            datas.dataInformada = DateTime.Parse("25/03/2022 17:00:00");
+=======
+           datas.dateHoje = DateTime.Now;
+            datas.dataInformada = DateTime.Parse("10/03/2022");
+>>>>>>> 2ca9ec86e1f39cdf2418564f8ae67f4f2a1fcad9
+            dias = datas.dateHoje - datas.dataInformada;
+            Console.WriteLine(datas.Mensagem(dias));
+            Console.ReadKey();
+        }
+    }
+}
